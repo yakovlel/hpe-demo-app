44b0e178b719493a1f3236ba77a9ba49f179b041
vaingato <vaingart@microfocus.com>
2019-03-17 17:35:52 +0200

    user story #2001

--- webapp/src/main/java/com/hp/devops/demoapp/Band.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Band.java
@@ -23,10 +23,10 @@ public class Band {
 		if (Math.random()   >= 0.5) {//                                                                                                                       v         2
 			throw new IllegalMonitorStateException("this is     a random exception");//                                                                     v          2
 		} //     																		                                                                    v            2
-		System.out.println("b and ");//   										                                                                          v           1
-		if (json.has("id"))  {//                                                                                                                          v     1
-			id = json.getInt("id");//                                                                                                                     v        1
-			if (json.has("name")) name =  json.getString("name");//                                                                                     v  1
+		System.out.println("b and ");//   										                                                                           v           1
+		if (json.has("id"))  {//                                                                                                                           v     1
+			id = json.getInt("id");//                                                                                                                       v        1
+			if (json.has("name")) name =  json.getString("name");//                                                                                       v  1
 			if (json.has("logo")) logo =  json.getString("logo");//                                                                                        v    1
 			if (json.has("song")) song =  json.getString("song");//                                                                                   v
 			if (json.has("votes")) votes  = json.getInt("votes");//                                                                                    v  1
