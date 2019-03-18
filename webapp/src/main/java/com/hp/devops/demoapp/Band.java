7cec755baa1c48949c6e236608784f5e7b696dd4
vaingato <vaingart@microfocus.com>
2019-03-18 13:42:13 +0200

    user story #2001

--- webapp/src/main/java/com/hp/devops/demoapp/Band.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Band.java
@@ -26,10 +26,10 @@ public class Band {
 		System.out.println("b and ");//   										                                                                             v           1
 		if (json.has("id"))  {//                                                                                                                            v     1
 			id = json.getInt("id");//                                                                                                                          v        1
-			if (json.has("name")) name =  json.getString("name");//                                                                                           v  1
-			if (json.has("logo")) logo =  json.getString("logo");//                                                                                              v    1
-			if (json.has("song")) song =  json.getString("song");//                                                                                      v
-			if (json.has("votes")) votes  = json.getInt("votes");//                                                                                       v  1
+			if (json.has("name")) name =  json.getString("name");//                                                                                            v  1
+			if (json.has("logo")) logo =  json.getString("logo");//                                                                                               v    1
+			if (json.has("song")) song =  json.getString("song");//                                                                                       v
+			if (json.has("votes")) votes  = json.getInt("votes");//                                                                                        v  1
 		} else {//                                                                                                                                            v           1
 			throw new InvalidParameterException("json must have an id property");//                                                                      v           2
 		}//                                                                                                                                                   v          2
