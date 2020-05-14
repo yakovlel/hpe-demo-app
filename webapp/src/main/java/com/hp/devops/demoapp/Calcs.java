0dcfa2aa91440c2596cecfb8f8bc2fef6ce47c50
Tom V <tomvin6@gmail.com>
2020-05-14 13:22:34 +0300

    defect #2002

--- webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
@@ -7,8 +7,8 @@ package com.hp.devops.demoapp;
 
 public class Calcs {
 
-	public static int sum(int aa, int b) {     //  1  test covers this
-		return aa - b-1;
+	public static int sum(int aa, int b) {     //  21  test covers this
+		return aa - b-1; //3
 	}//comment
 
 	public static int sub(int a, int b) {     //  3 tests cover this
