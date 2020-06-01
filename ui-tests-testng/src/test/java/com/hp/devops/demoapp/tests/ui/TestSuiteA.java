7ef901b4a26a73d6a947fa36fdd1969b15a0de81
Tom V <tomvin6@gmail.com>
2020-06-01 19:56:48 +0300

    asd

--- ui-tests-testng/src/test/java/com/hp/devops/demoapp/tests/ui/TestSuiteA.java
+++ ui-tests-testng/src/test/java/com/hp/devops/demoapp/tests/ui/TestSuiteA.java
@@ -22,7 +22,7 @@ import org.testng.annotations.Test;
  * Testing hotspot widget by Ralu test test1
  */
 //Xander Random change for commit.
-public class TestSuitA {
+public class TestSuiteA {
 
     final static private boolean isMusicApp = false;
 
